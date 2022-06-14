c1c54e543210b8b7769dcf69ffa66427636b044b
Sandy0825 <63026937+Sandy0825@users.noreply.github.com>
2022-06-14 16:01:13 +0900

    0614 upload js

--- js/common.js
+++ js/common.js
@@ -1,37 +1,6 @@
 $(function(){
   gnbMenu();
-
-  //let slideWidth = document.querySelector('.work_list > li').offsetWidth;
-  let tl = gsap.timeline({
-    scrollTrigger:{
-      trigger:'#work',
-      start:'top center',
-      scrub:true,
-      end:'+=300'
-    }
-  }),
-  slide = [...document.querySelectorAll('.work_list > li')]
-  // tl.to('#work .cont_tit',{
-  //   scale:0.8,
-  //   duration:1,
-  //   ease:'slow'
-  // })
-  tl.to(slide,{
-    xPercent: -95 * (slide.length - 1),
-    scrollTrigger: {
-        trigger: ".work_list",
-        start: "center center",
-        pin: true,
-        // horizontal: true,
-        // pinSpacing:false,
-        markers: true,
-        scrub: 1,
-        //snap: 1 / (slide.length - 1),
-        // base vertical scrolling on how wide the container is so it feels more natural.
-        // end: () => `+=${smallFactsContainer.offsetWidth}`
-        //end: () => `+=11200`
-    }
-  });
+  
 });
 
 function gnbMenu(){
@@ -73,4 +42,121 @@ function gnbMenu(){
         $(".js-gnb > li > a").removeClass("on");
         $(".js-gnb-bg").slideUp();
     });
+}
+
+function workSlide(){
+    gsap.registerPlugin(ScrollTrigger);
+    ScrollTrigger.matchMedia({
+        "(min-width:1025px) and (max-width:1399px)":function(){
+
+          let tl = gsap.timeline({
+            scrollTrigger:{
+              trigger:'#work',
+              start:'top center',
+              scrub:true,
+              //markers:true,
+              end:'+=300'
+            }
+          }),
+          slide = [...document.querySelectorAll('.work_list > li')]
+          // tl.to('#work .cont_tit',{
+          //   scale:0.8,
+          //   duration:1,
+          //   ease:'slow'
+          // })
+
+          tl.to(slide,{
+            xPercent: -100 * (slide.length),
+            scrollTrigger: {
+                trigger: ".work_list",
+                start: "center center",
+                pin: true,
+                // horizontal: true,
+                // pinSpacing:false,
+                markers: true,
+                scrub: 1,
+                //snap: 1 / (slide.length - 1),
+                // base vertical scrolling on how wide the container is so it feels more natural.
+                // end: () => `+=${smallFactsContainer.offsetWidth}`
+                end: () => `+=11200`
+            }
+          });
+        },
+        "(min-width:1400px)":function(){
+            let tl = gsap.timeline({
+            scrollTrigger:{
+              trigger:'#work',
+              start:'top center',
+              scrub:true,
+              //markers:true,
+              end:'+=300'
+            }
+          }),
+          slide = [...document.querySelectorAll('.work_list > li')]
+          // tl.to('#work .cont_tit',{
+          //   scale:0.8,
+          //   duration:1,
+          //   ease:'slow'
+          // })
+
+          tl.to(slide,{
+            xPercent: -100 * (slide.length),
+            scrollTrigger: {
+                trigger: ".work_list",
+                start: "center center",
+                pin: true,
+                // horizontal: true,
+                // pinSpacing:false,
+                markers: true,
+                scrub: 1,
+                //snap: 1 / (slide.length - 1),
+                // base vertical scrolling on how wide the container is so it feels more natural.
+                // end: () => `+=${smallFactsContainer.offsetWidth}`
+                end: () => `+=18200`
+            }
+          });
+        }
+    });
+   
+}
+function workSlide2(){
+    gsap.registerPlugin(ScrollTrigger);
+    ScrollTrigger.matchMedia({
+        "(min-width:1025px)":function(){
+
+          let tl = gsap.timeline({
+            scrollTrigger:{
+              trigger:'#work',
+              start:'top center',
+              scrub:true,
+              //markers:true,
+              end:'+=300'
+            }
+          }),
+          slide = [...document.querySelectorAll('.work_list > li')]
+          // tl.to('#work .cont_tit',{
+          //   scale:0.8,
+          //   duration:1,
+          //   ease:'slow'
+          // })
+
+          tl.to(slide,{
+            xPercent: -100 * (slide.length),
+            scrollTrigger: {
+                trigger: ".work_list",
+                start: "center center",
+                pin: true,
+                // horizontal: true,
+                // pinSpacing:false,
+                //markers: true,
+                scrub: 1,
+                //snap: 1 / (slide.length - 1),
+                // base vertical scrolling on how wide the container is so it feels more natural.
+                // end: () => `+=${smallFactsContainer.offsetWidth}`
+                end: () => `+=11200`
+            }
+          });
+        }
+    });
+   
 }
